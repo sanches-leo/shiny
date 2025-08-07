63d0eb4fed0adb823f03c86b009c13154e3bcd23
sanches-leo <leonardo2.sanches@usp.br>
2025-08-06 22:35:43 -0300

    fix permision bug

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Ensure the users directory exists and has the correct permissions
+mkdir -p /srv/shiny-server/lacen/www/users
+chown -R shiny:shiny /srv/shiny-server/lacen/www/users
+chmod -R 777 /srv/shiny-server/lacen/www/users
+
 # Start the cron service in the background
 service cron start
 
